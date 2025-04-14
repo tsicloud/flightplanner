bd3256fef8a6332119e6c8815a94b6d75e0d0f7b
tsicloud <48445826+tsicloud@users.noreply.github.com>
2025-04-14 12:49:10 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Non-Rev Flight Planner</h1>
+      <p>Setting up your travel tool...</p>
+    </div>
+  );
+}
+
+export default App;
